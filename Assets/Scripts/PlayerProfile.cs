c94275600c1f7296d54dc9550927340949441224
Hydofbl <hidayetfurkanbirel@gmail.com>
2023-07-07 15:32:21 +0300

    Basic scripts added.
    SaveData, PlayerProfile and SerializationManager.

---
+++ Assets/Scripts/PlayerProfile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerProfile
+{
+    public string playerName;
+    public int currency;
+    public int experience;
+}
